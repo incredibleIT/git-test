49328ab33385481cb5323659f154c1d6fbc41084
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2023-01-18 20:46:15 +0800

    HTML

---
+++ hello.js
@@ -0,0 +1,7 @@
+const pi = 3.14
+
+function calc(radius) {
+  return pi * radius * radius
+}
+
+console.log(calc(1.1))
---
+++ type.js
@@ -0,0 +1,6 @@
+let a = 1
+let b = '1'
+console.log(a === b)
+a = '1'
+a = 1
+console.log(a === b)
